38d62457141e220ad7d0047b412fb440c73ef8eb
Javad <ja7adr@gmail.com>
2021-02-05 16:38:32 +0330

    logger.py added

---
+++ src/logger.py
@@ -0,0 +1,33 @@
+import datetime
+import os
+
+
+class MetaSingleton(type):
+    _instance = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instance:
+            cls._instance[cls] = super().__call__(*args, **kwargs)
+        return cls._instance[cls]
+
+
+class Logger(metaclass=MetaSingleton):
+    def _write_file(self, type_log, msg: str):
+        if not os.path.exists('log'):
+            os.mkdir('log')
+        log_write = f'[{type_log}] [{str(datetime.datetime.now())}]: {msg}\n'
+        print(log_write)
+        with open(f'log/{datetime.date.today()}.log', 'a') as file_log:
+            file_log.write(log_write)
+
+    def info(self, msg):
+        self._write_file('Info', msg)
+
+    def error(self, msg):
+        self._write_file('Error', msg)
+
+    def warning(self, msg):
+        self._write_file('Warning', msg)
+
+    def debug(self, msg):
+        self._write_file('Debug', msg)
